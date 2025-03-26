e0b47139088ca3d5b170fac32be497114e546edc
Jason Azevedo <JCAzevedo@student.fullsail.edu>
2025-03-26 04:45:24 -0400

    f

--- express/server.js
+++ express/server.js
@@ -16,7 +16,7 @@ app.get("/nhl-schedule", async (req, res) => {
 
 app.get("/starting-goalies", async (req, res) => {
   try {
-    const response = await axios.get("https://www.dailyfaceoff.com/_next/data/OsOxQawjQHuUDdIU72CG4/starting-goalies.json");
+    const response = await axios.get("https://www.dailyfaceoff.com/_next/data/HZ5VbscUTkA2QB0O3MCQG/starting-goalies.json");
     res.json(response.data);
   } catch (error) {
     res.status(500).json({ error: "Failed to fetch data" });
